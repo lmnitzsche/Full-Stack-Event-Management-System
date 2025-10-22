e15fc88cabc7b160ac0b943574cc847a74f96dc0
lmnitzsche <logannitzsche1@gmail.com>
2025-10-21 20:25:29 -0500

    UE Improvement

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
         </Routes>
       </main>
       <Toaster 
-        position="top-right"
+        position="bottom-right"
         toastOptions={{
           duration: 4000,
           style: {
